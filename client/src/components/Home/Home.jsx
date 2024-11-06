b501b7b3cf4c48d532f29d98102fcca766d5d782
Arhangelov <arhangelov93@gmail.com>
2024-11-06 12:41:38 +0200

    edited create note handler

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,34 +1,56 @@
 import React, { useState } from 'react';
-import "./Home.css"
+import "./Home.css";
+//Service
 import { createNoteService } from '../../services/homeService';
 
+//Context
+import { useNotes } from '../../store/NotesContext';
+import NoteCard from '../NoteCard/NoteCard';
+
 const Home = () => {
-    const [note, setNote] = useState([]);
-    const [text, setText] = useState("");
+    const [notes, setNotes] = useNotes();
+    const [newNote, setNewNote] = useState({
+        id: null,
+        message: ""
+    });
 
     const onChangeHandler = (e) => {
-        setText(e.target.value);
+        setNewNote({
+            id: Math.floor(Math.random() * 100),
+            message: e.target.value
+        })
     }
 
     const createNote = async(e) => {
         e.preventDefault();
 
-        const newNote = {
-            id: Math.floor(Math.random() * 100),
-            message: text
-        }
-
-        createNoteService(newNote)
-        .then(notes => console.log(notes))
+        if (newNote.message.trim() === "") return;
+        
+        await createNoteService(newNote)
+        .then(updatedNotes => {
+            setNotes(updatedNotes);
+            setNewNote({
+                id: null,
+                message: ""
+            })
+        }).catch(err => console.error("Error creating note:", err))
         
     }
 
     return (
         <div className='home-container'>
             <form onSubmit={createNote}>
-                <textarea onChange={onChangeHandler} name="text" id="text" />
+                <textarea 
+                    onChange={onChangeHandler} 
+                    name="text" 
+                    id="text"
+                    value={newNote.message}
+                    rows="6"
+                    placeholder='Write a note...'
+                />
                 <input type="submit" value="Add Note" className='addBtn' />
             </form>
+            <NoteCard />
         </div>
     )
 }
