6bf1946d3701f69c9927617d131121444a31f3c5
Arhangelov <arhangelov93@gmail.com>
2024-11-06 12:37:48 +0200

    added home services

--- client/src/services/homeService.js
+++ client/src/services/homeService.js
@@ -1,9 +1,23 @@
 const url = "http://localhost:5000";
 
-export const createNoteService = async (newNote) => {
-    return fetch(url + `/create`, {
-        method: "POST",
-        headers: {"Content-Type": "äpplication/json"},
-        body:JSON.stringify(newNote)
+export const getAllNotesService = async () => {
+    return fetch(url + "/", {
+        method: "GET",
+        headers: {"Content-Type": "application/json"},
     }).then(res => res.json());
 }
+
+export const createNoteService = async (newNote) => {   
+    return fetch(url + "/", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body:JSON.stringify(newNote)
+    }).then(res => res.json());
+};
+
+export const deleteNoteService = async (noteId) => {
+    return fetch(url + `/delete-note/${noteId}`, {
+        method: "GET",
+        headers: {"Content-Type": "application/json"},
+    }).then(res => res.json());
+};
