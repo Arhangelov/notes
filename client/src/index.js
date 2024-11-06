332ab163624f27d9b2df04f99dac331300984d6d
Arhangelov <arhangelov93@gmail.com>
2024-11-06 12:36:21 +0200

    added note context

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router} from "react-router-dom";
 import App from './App';
 
+//Context
+import { NotesProvider } from './store/NotesContext';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <NotesProvider>
+        <App />
+      </NotesProvider>
     </Router>
   </React.StrictMode>
 );
