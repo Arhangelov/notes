3730944d6206d1ae80efe5e0ceb5b41bb4694038
Arhangelov <arhangelov93@gmail.com>
2024-11-09 22:49:51 +0200

    changed the context

--- client/src/store/NotesContext.js
+++ client/src/store/NotesContext.js
@@ -6,9 +6,10 @@ const initialState = [];
 
 export const NotesProvider = ({ children }) => {
     const [notes, setNotes] = useState(initialState);
+    const [isEditing, setIsEditing] = useState(false);
 
     return(
-        <NotesContext.Provider value={[notes, setNotes]}>
+        <NotesContext.Provider value={{ notes, setNotes, isEditing, setIsEditing }}>
             {children}
         </NotesContext.Provider>
     );
